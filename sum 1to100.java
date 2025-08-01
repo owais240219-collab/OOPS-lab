af6305cb309e14dfa6cdc89209768c280dc0762a
owais240219-collab <owais.240219@aids.ritchennai.edu.in>
2025-08-01 14:29:16 +0530

    Create sum 1to100.java

---
+++ sum 1to100.java
@@ -0,0 +1,16 @@
+package owais;
+
+public class numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0;
+		for(int i=1;i<=100;i++) {
+			a+=i;
+			}
+			System.out.println(a);
+			
+
+	}
+
+}
