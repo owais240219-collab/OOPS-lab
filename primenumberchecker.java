298f4112834c32d3bfb2d7569793e48fe65b4f11
owais240219-collab <owais.240219@aids.ritchennai.edu.in>
2025-08-10 11:37:31 +0530

    Update primenumberchecker.java

--- primenumberchecker.java
+++ primenumberchecker.java
@@ -1,1 +1,37 @@
+package primecheckerproject;
+
+import java.util.Scanner;
+
+public class Primecheck {
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false; // 0 and 1 are not prime numbers
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; // Divisible by a number other than 1 and itself
+            }
+        }
+
+        return true; // No divisors found, number is prime
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+
+        scanner.close();
+    }
+}
 
