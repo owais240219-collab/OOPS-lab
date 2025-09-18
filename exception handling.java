1bb0e9d2acec4894d1f9f6a50eaf7615db8cc91f
owais240219-collab <owais.240219@aids.ritchennai.edu.in>
2025-09-18 20:27:52 +0530

    Create exception handling.java

---
+++ exception handling.java
@@ -0,0 +1,25 @@
+package owais;
+import java.util.Scanner;
+
+public class exceptionhandling {
+
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println(" Owais Mohammed Khan");
+	        System.out.println(" 2117240070219");
+	        	        
+	        System.out.print("Enter the numerator: ");
+	        int numerator = scanner.nextInt();
+	        System.out.print("Enter the denominator: ");
+	        int denominator = scanner.nextInt();	        
+	        try {
+	            int result = numerator / denominator;  // Division operation	            
+	            System.out.println("Result: " + result);
+	        } catch (ArithmeticException e) {	           
+	            System.out.println("Exception handled: Cannot divide by zero.");
+	        }
+
+	        scanner.close();
+	    }
+	}
